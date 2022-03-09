75d6aa4b9e728d497b314e2fd7c66fbbc119e8bd
Hendrik Mans <hendrik@mans.de>
2022-03-09 21:01:00 +0100

    Get some boids ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { Boids } from "./Boids"
 
 const App = () => (
   <Canvas>
     <Boids />
+    <OrbitControls />
   </Canvas>
 )
 
--- src/Boids.tsx
+++ src/Boids.tsx
@@ -1,5 +1,17 @@
+import { PerspectiveCamera } from "@react-three/drei"
+import { IEntity, Tag } from "miniplex"
+import { createECS } from "miniplex/react"
+import { insideSphere } from "randomish"
 import { FC } from "react"
+import { Group, Object3D } from "three"
 import { makeInstanceComponents } from "./lib/Instances"
+
+type Entity = {
+  transform: Object3D
+  boid: Tag
+} & IEntity
+
+const ecs = createECS<Entity>()
 
 const Boid = makeInstanceComponents()
 
@@ -8,12 +20,31 @@ export const Boids: FC = () => {
     <>
       <ambientLight />
       <directionalLight />
+      <PerspectiveCamera position={(0, 0, 120)} makeDefault />
+
       <Boid.Root>
         <sphereGeometry />
         <meshStandardMaterial color="#eee" />
       </Boid.Root>
 
-      <Boid.Instance />
+      <ecs.Collection tag="boid" initial={300}>
+        {(entity) => (
+          <group ref={(group) => initializeBoidTransform(entity, group!)}>
+            <Boid.Instance />
+          </group>
+        )}
+      </ecs.Collection>
     </>
   )
+}
+
+const initializeBoidTransform = (entity: Entity, group: Group) => {
+  if (!group) {
+    ecs.world.removeComponent(entity, "transform")
+  } else {
+    ecs.world.addComponent(entity, "transform", group)
+
+    const pos = insideSphere()
+    group.position.set(pos.x * 100, pos.y * 100, pos.z * 100)
+  }
 }
