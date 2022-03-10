3765ebe84f0d797539a86f524a11c41ddecc6809
Hendrik Mans <hendrik@mans.de>
2022-03-10 17:01:56 +0100

    Fewer boids

--- src/Boids.tsx
+++ src/Boids.tsx
@@ -46,7 +46,7 @@ export const Boids: FC = () => {
         <meshStandardMaterial color="#eee" />
       </Boid.Root>
 
-      <ecs.Collection tag="boid" initial={3000}>
+      <ecs.Collection tag="boid" initial={2000}>
         {(entity) => (
           <group ref={(group) => initializeBoidTransform(entity, group!)}>
             <ecs.Component
